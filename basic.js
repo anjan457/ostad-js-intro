d116502739255c39a905b88d6b88820b2103a435
anjan457 <anjankundu392@gmail.com>
2023-02-04 21:14:40 +0600

    first commit

--- basic.js
+++
@@ -1,6 +0,0 @@
-var studentName = "ak";
-console.log(studentName);
-var studentAge = 24;
-var isStudent = true;//hi
-console.log(studentAge, isStudent)
-console.log(typeof studentAge, typeof isStudent);
